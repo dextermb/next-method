517a8bc91657b02259e47464f14ccd19673fa902
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2021-01-20 15:01:02 +0000

    Convert to object rather than function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const http = require('http')
 
 const isResponse = v => v instanceof http.ServerResponse
 
-module.exports = function (req, res) {
-  this._handlers = {}
-
-  this._add = (key, fn) => {
+module.exports = {
+  _handlers: {},
+  _add: function (key, fn) {
     let tmp = this._handlers[key]
 
     if (!Array.isArray(tmp)) {
@@ -21,65 +20,58 @@ module.exports = function (req, res) {
     this._handlers[key].push(fn)
 
     return this
-  }
+  },
+  use: function (fn) {
+    return this._add('*', fn)
+  },
+  get: function (fn) {
+    return this._add('get', fn)
+  },
+  post: function (fn) {
+    return this._add('post', fn)
+  },
+  patch: function (fn) {
+    return this._add('patch', fn)
+  },
+  put: function (fn) {
+    return this._add('put', fn)
+  },
+  delete: function (fn) {
+    return this._add('delete', fn)
+  },
+  end: function () {
+    return async (req, res) => {
+      const method = req.method.toLowerCase()
 
-  this.use = fn => (
-    this._add('*', fn)
-  )
+      // Check that handlers are set for method
+      if (Array.isArray(this._handlers[method])) {
+        const middleware = this._handlers['*']
+        const handlers = [].concat(middleware, this._handlers[method])
 
-  this.get = fn => (
-    this._add('get', fn)
-  )
+        // Run through all handlers
+        for (let i = 0; i < handlers.length; i++) {
+          const fn = handlers[i]
 
-  this.post = fn => (
-    this._add('post', fn)
-  )
+          if (typeof fn === 'function') {
+            const out = await fn(req, res)
 
-  this.patch = fn => (
-    this._add('patch', fn)
-  )
-
-  this.put = fn => (
-    this._add('put', fn)
-  )
-
-  this.delete = fn => (
-    this._add('delete', fn)
-  )
-
-  this.end = async () => {
-    const method = req.method.toLowerCase()
-
-    // Check that handlers are set for method
-    if (Array.isArray(this._handlers[method])) {
-      const middleware = this._handlers['*']
-      const handlers = [].concat(middleware, this._handlers[method])
-
-      // Run through all handlers
-      for (let i = 0; i < handlers.length; i++) {
-        const fn = handlers[i]
-
-        if (typeof fn === 'function') {
-          const out = await fn(req, res)
-
-          // Exit early if a response is returned
-          if (isResponse(out)) {
-            return
+            // Exit early if a response is returned
+            if (isResponse(out)) {
+              return
+            }
           }
         }
+
+        return
       }
 
-      return
+      const err = Error(`Missing handlers for '${method}' method`)
+
+      err.method = method
+      err.req = req
+      err.res = res
+
+      throw err
     }
-
-    const err = Error(`Missing handlers for '${method}' method`)
-
-    err.method = method
-    err.req = req
-    err.res = res
-
-    throw err
   }
-
-  return this
 }
