a742c95dc84853152306e71846714953d277d4a9
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2021-01-20 15:31:48 +0000

    Add next function - Remove babel runtime - Strip comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-const http = require('http')
-
-const isResponse = v => v instanceof http.ServerResponse
-
 module.exports = {
+  _CONTINUE: '__NM_CONTINUE',
   _handlers: {},
   _add: function (key, fn) {
     let tmp = this._handlers[key]
@@ -20,6 +17,9 @@ module.exports = {
     this._handlers[key].push(fn)
 
     return this
+  },
+  _next: function () {
+    return this._CONTINUE
   },
   use: function (fn) {
     return this._add('*', fn)
@@ -40,24 +40,22 @@ module.exports = {
     return this._add('delete', fn)
   },
   end: function () {
-    return async (req, res) => {
+    return (req, res) => {
       const method = req.method.toLowerCase()
 
       // Check that handlers are set for method
       if (Array.isArray(this._handlers[method])) {
         const middleware = this._handlers['*']
         const handlers = [].concat(middleware, this._handlers[method])
+        const next = this._next.bind(this)
 
         // Run through all handlers
         for (let i = 0; i < handlers.length; i++) {
           const fn = handlers[i]
 
           if (typeof fn === 'function') {
-            const out = await fn(req, res)
-
-            // Exit early if a response is returned
-            if (isResponse(out)) {
-              return
+            if (fn(req, res, next) !== this._CONTINUE) {
+              break
             }
           }
         }
